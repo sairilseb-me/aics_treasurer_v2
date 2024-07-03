8c77db3526fe0925cc308c97d6e318a0bf422f9c
Sai Seberiaga <cyrilseberiaga@gmail.com>
2024-07-03 11:17:20 +0800

    installed backend files

---
+++ frontend/src/views/pages/Homepage.vue
@@ -0,0 +1,62 @@
+<template>
+    <Card class="w-100">
+        <template #content>
+                <div class="flex justify-end">
+                    <Button class="border border-solid border-slate-500 bg-sky-600 py-2 px-3 text-white">
+                        <i class="pi pi-file"></i>
+                        <span class="ml-2">Export  to Excel</span>
+                    </Button>
+                    <InputGroup class="w-[20rem]">
+                        <span class="p-inputgroup-addon">
+                            <i class="pi pi-search"></i>
+                        </span>
+                        <input-text placeholder="Search" class="border border-solid border-slate-400 py-2 px-3"></input-text>
+                    </InputGroup>
+                </div>
+            <data-table class="border rounded mt-5">
+               <Column v-for="header in headers" :key="header.field" :field="header.field" :header="header.header"></Column>
+            </data-table>
+        </template>
+    </Card>
+</template>
+
+<script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
+import Card from 'primevue/card';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import Button from 'primevue/button';
+import { ref } from 'vue';
+export default {
+    components: {
+        DataTable,
+        Column,
+        InputText,
+        Card,
+        InputGroup,
+        InputGroupAddon,
+        Button,
+    },
+    setup() {
+        
+        const headers = ref([
+            {field: 'FullName', header: 'Full Name'},
+            {field: 'TypeOfAssistance', header: 'Type Of Assistance'},
+            {field: 'Category', header: 'Category'},
+            {field: 'SourceOfFund', header: 'Source of Fund'},
+            {field: 'Amount', header: 'Amount'},
+            {field: 'ReceivedDate', header: 'Received Date'},
+            {field: 'Mode', header: 'Mode'},
+            {field: 'actions', header: 'Actions'}
+        ])
+        return {
+            // variables
+            headers,
+
+        }
+
+    },
+}
+</script>
---
+++ frontend/src/views/pages/TestPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div className="card">
+        <h5>This is a test Page</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nostrum impedit neque. Quam, laudantium facere eaque nisi necessitatibus placeat cupiditate assumenda tempore hic, itaque quo, iure exercitationem velit fuga sed.
+        Commodi repellendus esse provident corrupti nesciunt repudiandae ducimus sequi rerum facere ipsa! Corrupti, commodi, dolore deleniti illum officiis voluptas eum corporis numquam, itaque maxime magni ut earum provident sint deserunt!
+        Ratione quae vero necessitatibus culpa dolorem assumenda aliquid! Maiores quia illo cupiditate debitis quidem error nemo rerum cumque perferendis. Aperiam itaque modi consequuntur necessitatibus aut minus hic eum blanditiis ad!
+        Sint perferendis, odit voluptatem possimus qui atque voluptatibus voluptatum nihil quos harum tempora minima, iure iusto, odio labore libero! Et commodi expedita possimus enim veritatis iste quaerat vel delectus odit?
+        Unde rerum optio, itaque exercitationem expedita facilis magnam saepe deleniti necessitatibus delectus odit a ipsum veritatis vel. Temporibus doloribus est corporis. Obcaecati blanditiis eum consectetur reiciendis iste deserunt saepe libero?</p>
+    </div>
+</template>
